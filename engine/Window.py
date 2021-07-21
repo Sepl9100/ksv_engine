58e21d8c564d9c89c98bd65b6457417dc0fbc178
Sepl9100 <77353871+Sepl9100@users.noreply.github.com>
2021-07-21 20:34:49 +0200

    small update

--- engine/Window.py
+++ engine/Window.py
@@ -1,17 +1,34 @@
 from GLOBAL import *
 from levels.main_menu import *
+from engine.text_box import *
 
 
 class Window:
     def __init__(self):
-
+        self.window = self
+        self.xoff = 0
+        self.yoff = 0
         self.width = 1000
         self.height = 500
         self.flags = pg.DOUBLEBUF | pg.HWSURFACE
         self.screen = pg.display.set_mode((self.width, self.height), self.flags, 32)
-        pg.display.flip()
+        self.intro = TextBox(0, 0, "Powered by the KSV Engine", self, APP_["FONT_1"], COLS["WHITE"])
+        self.intro.sprite.x = self.width//2 - self.intro.sprite.width // 2
+        self.intro.sprite.y = self.height//2 - self.intro.sprite.height // 2
+        self.intro.sprite.draw()
+        self.update()
+        sleep(2)
+        self.intro.remove()
+
+
 
     def update(self):
         pg.display.set_caption(f"FPS: {round(APP_['GAMECLOCK'].get_fps(), 1)} - PyPong {VERSION} by {AUTHOR}")
         pg.display.flip()
 
+
+    def load_background(self, path):
+        source_scale = get_scale(path)
+        img = pg.image.load(path).convert_alpha()
+        return pg.transform.scale(img, (int(source_scale[0]*(self.width/source_scale[0])),
+                                              int(source_scale[1]*(self.height/source_scale[1]))))
