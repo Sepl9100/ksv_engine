33e91d82946d431211efad9c69d0cb09fa26e3da
Sebastian R <77353871+Sepl9100@users.noreply.github.com>
2022-01-28 20:25:18 +0100

    pfusch

--- engine/ai.py
+++ engine/ai.py
@@ -36,6 +36,8 @@ class AI:
         self.go_back = False
         if not self.back:
             self.repeat = False
+        if self.repeat:
+            self.disappear = False
 
     def update(self, speed):
         self.speed = speed
@@ -50,17 +52,18 @@ class AI:
                         self.needs_update = False
                         if not self.back:
                             if self.disappear:
-                                self.entity.sprite.fill_image("DATA/textures/transparent.png", True)
+                                self.entity.sprite.make_invisible()
             else:
                 self.entity.place(self.path[self.counter][0], self.path[self.counter][1])
                 self.counter -= 1
-                if self.rect.x <= self.x_original and self.rect.y <= self.y_original:
+                if self.rect.x == self.x_original and self.rect.y == self.y_original:
                     self.go_back = False
                     if not self.repeat:
                         self.needs_update = False
                         if self.disappear:
-                            self.entity.sprite.fill_image("DATA/textures/transparent.png", True)
+                            self.entity.sprite.make_invisible()
+            if self.counter < 0:
+                self.counter = 0
             if self.speed > 1:
                 self.speed -= 1
                 self.update(self.speed)
-
